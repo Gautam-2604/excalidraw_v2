03e06f8798eaf56253c2dd3818cbb89ebb5a5c10
Gautam-2604 <gaurisariag@gmail.com>
2025-07-09 01:38:35 +0530

    added packages and middleware

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,11 +1,18 @@
 import express from "express"
 import jwt from "jsonwebtoken"
 import { middleware } from "./middleware";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "backend-common/config"
+import { createRoomSchema, createUserSchema, signInSchema } from "common/types";
 
 const app = express()
 
 app.post("/signup",(req,res)=>{
+    const data =createUserSchema.safeParse(req.body)
+    if(!data.success){
+        return res.json({
+            message:"Chekc inputs"
+        })
+    }
     console.log("Sign up");
     jwt.sign({req},JWT_SECRET)
 
@@ -13,11 +20,23 @@ app.post("/signup",(req,res)=>{
 })
 
 app.post("/signin",(req,res)=>{
+    const data =signInSchema.safeParse(req.body)
+    if(!data.success){
+        return res.json({
+            message:"Chekc inputs"
+        })
+    }
     console.log("Sign up");
     
 })
 
 app.post("/room",middleware,(req,res)=>{
+    const data =createRoomSchema.safeParse(req.body)
+    if(!data.success){
+        return res.json({
+            message:"Chekc inputs"
+        })
+    }
     console.log("Create Room");
     
 })
